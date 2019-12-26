d0245334307c3494181b177e8c52a00d99f0d38e
DevRappers <devrappers@gmail.com>
2019-12-26 15:15:00 +0900

    shallowEqual을 사용해서 useSelector최적화

--- one/src/containers/CounterContainer.js
+++ one/src/containers/CounterContainer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Counter from '../components/Counter';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { increase, decrease, setDiff } from '../modules/counter';
 
 function CounterContainer() {
-	const { number, diff } = useSelector((state) => ({
-		number: state.counter.number,
-		diff: state.counter.diff
-	}));
+	// shallowEqual을 써주지 않았을경우에는 number만 바뀌어도 diff도 리렌더링을 한다.
+	// shallowEqual을 사용하면 최적화 가능
+	const { number, diff } = useSelector(
+		(state) => ({
+			number: state.counter.number,
+			diff: state.counter.diff
+		}),
+		shallowEqual
+	);
 
 	const dispatch = useDispatch();
 
