14b2fbbb7d0898d50de7dfbd7c3f81368965ff98
DevRappers <devrappers@gmail.com>
2019-12-27 11:26:18 +0900

    redux-thunk로 Promise다루기

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import './App.css';
-import CounterContainer from './container/CounterContainer';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
 
 function App() {
-	return <CounterContainer />;
+	return (
+		<div>
+			<Route path="/" component={PostListPage} exact />
+			<Route path="/:id" component={PostPage} />
+		</div>
+	);
 }
 
 export default App;
