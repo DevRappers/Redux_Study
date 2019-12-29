325ce5a0535d9c735b9f6f8bd3b29336fd8f0cd1
DevRappers <devrappers@gmail.com>
2019-12-29 17:11:27 +0900

    redux-saga 라우터 연동

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -14,7 +14,11 @@ import { createBrowserHistory } from 'history';
 import createSagaMiddleware from 'redux-saga';
 
 const customHistory = createBrowserHistory();
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware({
+	context: {
+		history: customHistory
+	}
+});
 const store = createStore(
 	rootReducer,
 	// logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
